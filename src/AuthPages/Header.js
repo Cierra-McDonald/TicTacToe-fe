4e64ae2e41bda214f723075820fc9674cadf7517
Cierra-McDonald <cierralauren15@gmail.com>
2021-03-17 14:00:35 -0700

    components created

---
+++ src/AuthPages/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Header extends Component {
+    render() {
+        return (
+            <div className="main-header">
+                Hello I am the header!
+            </div>
+        )
+    }
+}
